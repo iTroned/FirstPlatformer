da645f16fff222c2994e406d69f36c1d5e02114b
iTroned <72600583+iTroned@users.noreply.github.com>
2022-01-13 12:20:42 +0100

    Upload

---
+++ Assets/_Scripts/BirdScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BirdScript : MonoBehaviour
+{
+    bool isFlying = false;
+    public float sight = 10;
+    Animator controller;
+    void Start()
+    {
+        controller = gameObject.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(isFlying == true)
+        {
+            controller.SetBool("IsFlying", true);
+        }
+        else
+        {
+            controller.SetBool("IsFlying", false);
+        }
+        if(Vector2.Distance(transform.position, GameObject.Find("Bombie").transform.position) <= sight)
+        {
+            isFlying = true;
+
+        }
+        else
+        {
+            isFlying = false;
+        }
+    }
+}
---
+++ Assets/_Scripts/CharCon.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharCon : MonoBehaviour
+{
+    SpriteRenderer render;
+    Animator controller;
+    Rigidbody2D ri;
+    public float speed = 60f;
+    bool isRunning = false;
+    bool isGrounded = false;
+    public float jumpForce;
+    public float rayLength;
+    void Start()
+    {
+        ri = gameObject.GetComponent<Rigidbody2D>();
+        controller = gameObject.GetComponent<Animator>();
+        render = gameObject.GetComponent<SpriteRenderer>();
+        Physics2D.queriesStartInColliders = false;
+    }
+
+    // Update is called once per frame
+    private void FixedUpdate()
+    {
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, transform.up * -1, rayLength);
+        if(hit.collider != null)
+        {
+            isGrounded = true;
+        }
+        else
+        {
+            isGrounded = false;
+        }
+    }
+    void Update()
+    {
+        
+
+        
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            ri.AddForce(transform.right * speed, ForceMode2D.Force);
+            isRunning = true;
+            render.flipX = false;
+            
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            ri.AddForce(transform.right * speed * -1, ForceMode2D.Force);
+            isRunning = true;
+            render.flipX = true;
+        }
+        else
+        {
+            isRunning = false;
+        }
+        if(isRunning == true)
+        {
+            controller.SetBool("Running", true);
+        }
+        else
+        {
+            controller.SetBool("Running", false);
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Jump();
+        }
+    }
+    void Jump()
+    {
+        if (isGrounded == true)
+        {
+            controller.SetTrigger("Jump");
+            ri.AddForce(transform.up * jumpForce);
+        }
+    }
+   
+}
